773a9713a1d5ba98b7b1f43037a45e348ae3cdf7
Mike Jorgensen <mikejorgensen@gmail.com>
2012-07-12 15:22:07 -0500

    prompt for directory containing IPAs to be resigned. Add some error handling around arguments.

--- resignwrapper.sh
+++ resignwrapper.sh
@@ -1,3 +1,13 @@
 #! /bin/sh
 
-find ~/Desktop/ -type f -name "*.ipa" -exec ./unar {} \;
+if [ $# = 0 ]; then
+	echo "Please provide a path where IPA files should be found"
+	exit 1
+fi
+	
+if [ -d $1 ]; then 
+	find "$1" -type f -name "*.ipa" -exec ./unar {} -f \;
+else
+	echo "directory \"$1\" not found"
+	exit 1
+fi
