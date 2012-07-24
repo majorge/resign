4554cc9342ac2f61f7b8a592c02331f3daccd4ec
Mike Jorgensen <mikejorgensen@gmail.com>
2012-07-24 10:23:42 -0500

    Deleted dumb comments

--- resign-wrapper.rb
+++ resign-wrapper.rb
@@ -6,7 +6,6 @@ OptionParser.new do |opts|
 
   opts.on("-d", "--directory [dir path]", "Directory containing IPAs to resign") do |d|
     options[:directory] = d
-#    puts d
   end
 
   opts.on("-p", "--prov_profile_path [provisioning profile path", "Path to provisioning profile to resign with") do |d|
@@ -15,19 +14,14 @@ OptionParser.new do |opts|
   
 end.parse!
 
-#  puts options[:directory]
 dir = File.expand_path(options[:directory])
 apps = Dir.foreach(dir) do |file|
   next if not file =~ /.*\.ipa/i
 
-#  IO.popen("./unar \"#{options[:directory]}#{file}\"") { |io| while (line = io.gets) do puts line end }
-#  puts file
   system("./unar -force-overwrite \"#{dir}/#{file}\"")
 
   puts Dir.glob("Payload/*.app") {|i| system("./resign.rb --prov_profile_path #{options[:prov_profile_path]} --app_path \"#{File.expand_path(i)}\" --developerid \"iPhone Distribution: Mallinckrodt, Inc.\"") }
   
-  
-
   Dir.glob("Payload/*.app") do |p|
 #    File.chmod(777, p)
     system("rm -rf \"#{File.expand_path(p)}\"")
